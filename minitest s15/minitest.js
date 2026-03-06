8f0b6b4e6b24f7b46cab2a9bec60615f85e0b221
Thinh53124 <thinh531240958@gmail.com>
2026-03-06 15:59:11 +0700

    Update minitest.js

--- minitest s15/minitest.js
+++ minitest s15/minitest.js
@@ -1,41 +1,36 @@
 let login = [
-    {email: `thinh531240958@gmail.com`, password: `12345`, rePassword: `12345`},
-    {email: `rikkeiedu@gmail.com`, password: `53124`, rePassword: `53124`},
+    {email: "thinh531240958@gmail.com", password: "12345", rePassword: "12345"},
+    {email: "rikkeiedu@gmail.com", password: "53124", rePassword: "53124"},
 ];
 
-let loginList = document.getElementById(login);
-function renderLogin() {
-    let str = "";
-    for (let i = 0; i < login.length; i++) {
-        str = `
-        <p>${login[i].email}</p>
-        <input type="email" name="email">
-        <p>${login[i].password}</p>
-        <input type="password" name="password">
-        <p>${login[i].rePassword}</p>
-        <input type="password" name="password">
-        <br>
-        <button type="submit">Đăng Ký</button>
-        `;
-    };
-    document.getElementById("login").innerHTML = str;
-}
-
 function addInfo() {
-    document.getElementById(login).value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let rePassword = document.getElementById("rePassword").value;
+
     let obj = {
         email: email,
         password: password,
-        rePassword: rePassword,
+        rePassword: rePassword
     }
+
     login.push(obj);
     console.log(login);
 }
 
 function validate() {
-    if (password == rePassword) {
-        addInfo(login);
-    }else{
-        
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let rePassword = document.getElementById("rePassword").value;
+    let checkEmail = login.find(user => user.email === email);
+
+    if (checkEmail) {
+        alert("Email đã tồn tại");
+        return;
+    }
+    if (password === rePassword) {
+        addInfo();
+    } else {
+        alert("rePassword đang không trùng password");
     }
 }
